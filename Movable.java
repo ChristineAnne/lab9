903e16232f520f8bd0e96f8a5f4eea1f971b0ff8
Christine Anne Catubig <christineannecatubig@gmail.com>
2016-10-11 10:13:36 +0800

    Add files via upload

---
+++ Movable.java
@@ -0,0 +1,8 @@
+package Lab9;
+
+public interface Movable{
+	void moveUp();
+	void moveDown();
+	void moveLeft();
+	void moveRight();
+}
---
+++ MovableCircle.java
@@ -0,0 +1,36 @@
+package Lab9;
+
+public class MovableCircle implements Movable{
+	private int radius;
+	private MovablePoint center;
+
+	public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius){
+		this.x = x;
+		this.y = y;
+		this.xSpeed = xSpeed;
+		this.ySpeed = ySpeed;
+		this.radius = radius;
+	}
+
+	public String toString(){
+		return "";
+	}
+
+	public void moveUp(){
+
+	}
+
+	public void moveDown(){
+
+	}
+
+	public void moveLeft(){
+
+	}
+
+	public void moveRight(){
+
+	}
+
+
+}
