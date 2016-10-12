c5377fa41b0e55b6dc9737634f996f352b0bc9bd
Christine Anne Catubig <christineannecatubig@gmail.com>
2016-10-12 12:43:55 +0800

    Add files via upload

---
+++ part2/ResizableCircle.java
@@ -0,0 +1,14 @@
+
+package lab9.part2;
+
+public class ResizableCircle extends Circle implements Resizable {
+    
+    public ResizableCircle(double radius){
+        super(radius);
+    }
+    
+    @Override
+    public void resize(int percent){
+        
+    }
+}
---
+++ part2/TestCircle.java
@@ -0,0 +1,12 @@
+package lab9.part2;
+
+/**
+ *  created by Christine Anne Catubig
+ */
+public class TestCircle {
+    public static void main(String[] args) {
+        GeometricObject object1 = new Circle(5);
+        System.out.println("Area = " + object1.getArea());
+        System.out.println("Perimeter = " + object1.getPerimeter());           
+    }
+}
